e41ebf97e102b96cf5d781afa20e8369cc278c60
meggrasse <meggrasse@u.northwestern.edu>
2018-04-27 20:03:59 -0500

    iOS templating.

---
+++ chillout/chillout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chillout
+//
+//  Created by Meg on 4/27/18.
+//  Copyright © 2018 meggrasse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

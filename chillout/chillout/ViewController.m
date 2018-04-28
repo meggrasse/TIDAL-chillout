d617c182bb08aed8cd75ab87bd19cc71efcdbf14
meggrasse <meggrasse@u.northwestern.edu>
2018-04-27 20:55:56 -0500

    Implements authoriziation and can add a track to a playlist.

--- chillout/chillout/ViewController.m
+++ chillout/chillout/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import <MediaPlayer/MediaPlayer.h>
+
 @interface ViewController ()
 
 @end
@@ -16,7 +18,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // request authorization to access the user's media library
+    [MPMediaLibrary requestAuthorization:^(MPMediaLibraryAuthorizationStatus status) {
+        if (status != MPMediaLibraryAuthorizationStatusAuthorized) {
+            // if we don't get access, abort
+            NSLog(@"Need authorization");
+            exit(0);
+        }
+    }];
+    
+    //  uncomment to test each method
+//    [self addCurrentTrackToPlaylistTest];
 }
 
 
@@ -25,5 +38,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)addCurrentTrackToPlaylist:(MPMediaPlaylist *)playlist {
+    NSString *currentTrackProductID = MPMusicPlayerController.systemMusicPlayer.nowPlayingItem.playbackStoreID;
+    [playlist addItemWithProductID:currentTrackProductID completionHandler:nil];
+}
+
+#pragma mark - Tests
+- (void)addCurrentTrackToPlaylistTest {
+    
+    MPMediaPlaylistCreationMetadata *playlistData = [[MPMediaPlaylistCreationMetadata alloc] initWithName:@"addCurrentTrackToPlaylistTest"];
+    [MPMediaLibrary.defaultMediaLibrary getPlaylistWithUUID: [NSUUID UUID]
+                       creationMetadata:playlistData
+                      completionHandler:^(MPMediaPlaylist *playlist, NSError *error) {
+                          [self addCurrentTrackToPlaylist:playlist];
+                      }];
+}
 
 @end

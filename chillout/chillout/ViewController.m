2e7d899e408eb26cdb3f0aa9a1a2d0ea1b2ac66c
meggrasse <meggrasse@u.northwestern.edu>
2018-04-29 10:17:02 -0500

    Added UI.

--- chillout/chillout/ViewController.m
+++ chillout/chillout/ViewController.m
@@ -28,6 +28,50 @@
         }
     }];
     
+    // load UI
+    
+    // make "chillout" label
+    UILabel *title = [[UILabel alloc] init];
+    title.text = @"chillout";
+    title.textAlignment = NSTextAlignmentCenter;
+    title.font = [UIFont systemFontOfSize:36 weight:UIFontWeightSemibold];
+    title.translatesAutoresizingMaskIntoConstraints = NO;
+    // add to the view
+    [self.view addSubview:title];
+    
+    // use autolayout for position of "chillout"
+    NSArray<NSLayoutConstraint *> *constraints = @[
+                                                   [title.leftAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leftAnchor],
+                                                   [title.rightAnchor constraintEqualToAnchor:self.view.safeAreaLayoutGuide.rightAnchor],
+                                                   [title.centerYAnchor constraintEqualToAnchor:self.view.centerYAnchor constant:-100],
+                                                   ];
+    
+    
+    [NSLayoutConstraint activateConstraints:constraints];
+    
+    // make "start listening" button
+    UIButton *listen = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    // not autolayout
+    listen.frame = CGRectMake(0, 0, 50, 50);
+    listen.center = CGPointMake(self.view.center.x, self.view.center.y + 20);
+    
+    // button title label
+    [listen setTitle:@"▶︎" forState:UIControlStateNormal];
+    listen.titleLabel.font = [UIFont systemFontOfSize:25];
+    [listen setTitleColor:UIColor.blackColor forState:UIControlStateNormal];
+    listen.titleLabel.textAlignment = NSTextAlignmentCenter;
+    listen.backgroundColor = UIColor.clearColor;
+    
+    // black border
+    listen.layer.masksToBounds = YES;
+    listen.layer.cornerRadius = listen.frame.size.width / 2;
+    listen.layer.borderWidth = 1;
+    listen.layer.borderColor = UIColor.blackColor.CGColor;
+    
+    // add to the view
+    [self.view addSubview:listen];
+    
     //  uncomment to test each method
 //    [self addCurrentTrackToPlaylistTest];
 }
